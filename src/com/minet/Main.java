6f7adae622a9f59048ac4712a15a8fe159fca5a2
mumtaz1000 <shereenfatima1000@gmail.com>
2022-09-19 15:30:49 +0200

    C04: Multiple user chat channel is created

--- src/com/minet/Main.java
+++ src/com/minet/Main.java
@@ -1,10 +1,20 @@
 package com.minet;
 
+import com.minet.chat.Client;
+import com.minet.chat.Server;
 import com.minet.homeMenu.HomeMenu;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        new HomeMenu();
+    public static void main(String[] args) throws IOException {
+        //new HomeMenu();
+
     }
+
+
 
 }
